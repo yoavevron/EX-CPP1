933b7f71ad7c5b5913c30263e509f680a615b7f5
yoavevron <yoavev2020@gmail.com>
2025-01-03 11:19:52 +0200

    initialize project

---
+++ cmake-build-wsl/CMakeFiles/EX_CPP1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EX_CPP1.dir/link.d"
+  "CMakeFiles/EX_CPP1.dir/main.cpp.o"
+  "CMakeFiles/EX_CPP1.dir/main.cpp.o.d"
+  "EX_CPP1"
+  "EX_CPP1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EX_CPP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
